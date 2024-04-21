2a592a0bb7152fdf57eac1b01ba328c9b57b51b8
sukaruta <sukaruta12@gmail.com>
2024-04-22 00:27:23 +0800

    b

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from ursina import *
-from panda3d.ai import AIWorld, AICharacter
 from prefabs.material import Material
 from prefabs.entity import cEntity
 from prefabs.light import SpotLight
@@ -8,6 +7,7 @@ from prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
 player = FirstPersonController()
+player.position = (5, 0, 5)
 flashlight = cEntity()
 shader = Shader.load(Shader.GLSL, "assets/shaders/vertex.vert", "assets/shaders/SpotFragment.frag")
 flashlight_light = SpotLight(color=color.white, rotation=player.camera_pivot.rotation)
@@ -25,18 +25,17 @@ enemy.set_material(material69)
 material2 = Material()
 material2.texture = load_texture("assets/textures/chipping-painted-wall_albedo")
 material2.specular_map = load_texture("assets/textures/chipping-painted-wall_metallic")
-material2.texture_scale = Vec2(2, 2)
-walls = cEntity(model="assets/objects/testroom2.obj", scale=50, position=(10, 0, 10), collider="mesh", shader=shader)
+material2.texture_scale = Vec2(100, 100)
+walls = cEntity(model="assets/objects/imcummingmaze.obj", scale=6, position=(0, 0, 0), collider="mesh", shader=shader)
 walls.set_material(material2)
 
 material3 = Material()
 material3.texture = load_texture("assets/textures/floortile.jpg")
 material3.specular_map = load_texture("assets/textures/floortile.jpg")
-floor = cEntity(model="testroom2_floor.obj", collider="box", scale=(50, 50, 50))
+floor = cEntity(model="plane", collider="box", scale=80, shader=shader, position=(0, 0.2, 0), rotation=(180, 180, 0), double_sided=True)
 floor.set_material(material3)
-floor.shader = shader
 
-roof = cEntity(model="plane", texture="grass", collider="box", scale=(100, 100, 100), position=(0, 7, 0), shader=shader)
+roof = cEntity(model="plane", texture="grass", collider="box", scale=80, position=(0, 7, 0), shader=shader)
 roof.rotation = (180, 180, 0)
 
 flashlight_light.update_values()
