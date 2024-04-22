f656a16ce7c764545eaa6e13ff6bb89f9adbd944
sukaruta <sukaruta12@gmail.com>
2024-04-22 12:55:50 +0800

    Add basic functioning raycast vision

--- assets/scripts/BaseEnemy.py
+++ assets/scripts/BaseEnemy.py
@@ -2,11 +2,12 @@ from ursina import *
 from prefabs.entity import cEntity
 
 
-class BaseEnemy(cEntity):
+class BaseEnemy(cEntity, Entity):
     def __init__(self, **kwargs):
         super().__init__()
         self.speed = 5
         self.gravity = 1
+        self.pursuing_player = False
 
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -17,11 +18,12 @@ class BaseEnemy(cEntity):
         direction_to_player.normalize()
         self.position += direction_to_player * 0.02
         self.look_at_2d(self.target.position, 'y')
-        vision = raycast((self.world_position.x, 0, self.world_position.z) + (self.up.x, self.target.height - 1.5, self.up.z), self.forward, distance=50,
-                         traverse_target=self.target, debug=True)
-        if vision.hit:
-            print("Seen")
-            print(vision.entity)
+        vision = raycast((self.world_position.x, 0, self.world_position.z) + (self.up.x, self.target.height - 1.3, self.up.z), self.forward, distance=10, traverse_target=self.target, ignore=[self], debug=True)
+        #print("Vision hit: " + str(vision.hit))
+        #print("Is intersecting wall: " + str(self.walls not in vision.entities))
+        #print("Pursuing player: " + str(self.pursuing_player))
+        if (vision.hit and (self.walls not in vision.entities)) and not self.pursuing_player:
+            print("Cooked")
         collision_info = self.intersects(self.walls)
         if collision_info.hit:
             collision_normal = collision_info.normal.normalized()
