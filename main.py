5be143a0075867b1d5f21618869df5dbb2a73020
sukaruta <sukaruta12@gmail.com>
2024-04-22 09:41:22 +0800

    refactor: Code cleanup

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from assets.scripts.BaseEnemy import BaseEnemy
 from prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
-player = FirstPersonController()
+player = FirstPersonController(model="sphere", collider="mesh")
 player.position = (5, 0, 5)
 flashlight = cEntity()
 shader = Shader.load(Shader.GLSL, "assets/shaders/vertex.vert", "assets/shaders/SpotFragment.frag")
@@ -16,12 +16,6 @@ flashlight_light.model = None
 flashlight_light.setShader(shader)
 flashlight_light.add_script(SmoothFollow(player, offset=(0, player.height, 0)))
 
-material69 = Material()
-material69.texture = load_texture("assets/textures/kimmonster.png")
-material69.specular_map = load_texture("assets/textures/kimmonster.png")
-enemy = BaseEnemy(model="quad", shader=shader, position=(2, 3, 0), double_sided=True, scale=6, collider="mesh")
-enemy.set_material(material69)
-enemy.collider = 'box'
 
 material2 = Material()
 material2.texture = load_texture("assets/textures/chipping-painted-wall_albedo")
@@ -33,28 +27,26 @@ walls.set_material(material2)
 material3 = Material()
 material3.texture = load_texture("assets/textures/floortile.jpg")
 material3.specular_map = load_texture("assets/textures/floortile.jpg")
-floor = cEntity(model="plane", collider="box", scale=80, shader=shader, position=(0, 0.2, 0), rotation=(180, 180, 0), double_sided=True)
+floor = cEntity(model="plane", collider="mesh", scale=80, shader=shader, position=(0, 0.2, 0), rotation=(180, 180, 0), double_sided=True)
 floor.set_material(material3)
 
-roof = cEntity(model="plane", texture="grass", collider="box", scale=80, position=(0, 7, 0), shader=shader)
+roof = cEntity(model="plane", texture="grass", collider="mesh.", scale=80, position=(0, 7, 0), shader=shader)
 roof.rotation = (180, 180, 0)
 
+material69 = Material()
+material69.texture = load_texture("assets/textures/kimmonster.png")
+material69.specular_map = load_texture("assets/textures/kimmonster.png")
+enemy = BaseEnemy(model="quad", shader=shader, position=(2, 3, 0), double_sided=True, scale=(6, 6, material69.texture.width), collider="mesh", target=player, walls=walls)
+enemy.set_material(material69)
+enemy.collider = "box"
+
 flashlight_light.update_values()
+
 
 def update():
     flashlight_light.direction = camera.forward.normalized()
     flashlight_light.update_values()
-    direction_to_player = player.position - enemy.position
-    distance_to_player = direction_to_player.length()
-    direction_to_player.y = 0
-    direction_to_player.normalize()
-    enemy.position += direction_to_player * 0.03
-    enemy.look_at_2d(player.position, 'y')
-    collision_info = enemy.intersects(walls)
-    if collision_info.hit:
-        collision_normal = collision_info.normal.normalized()
-        slide_direction = Vec3(collision_normal.x, 0, collision_normal.z)
-        enemy.position += slide_direction * 0.09
+    enemy.target = player
 
 
 Audio("assets/sfx/burningmemory.ogg").play()
