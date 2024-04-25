e800157246638e5f3f3c60a3818061662bdcc7c5
sukaruta <sukaruta12@gmail.com>
2024-04-25 10:57:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def init_game():
     invoke(start_game, delay=7)
 
 
-app = Ursina(title="EotH", icon="assets/textures/echoes.ico", borderless=False)
+app = Ursina(title="EotH", icon="assets/textures/echoes.ico", borderless=True, development_mode=False, fullscreen=True)
 menu_video_sound = Audio("assets/sfx/neon_sounds.mp3")
 menu_video_sound.loop = True
 menu_video_sound.play()
